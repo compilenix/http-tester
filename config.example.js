bce06931a4587fb3a659bcdd86dff06cb64273cc
Compilenix <compilenix@gmail.com>
2018-06-10 03:31:56 +0200

    break working prototype into functions and add content-encoding support

--- config.example.js
+++ config.example.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 const path = require('path')
 const http = require('http')
+const cheerio = require('cheerio')
 /* eslint-enable no-unused-vars */
 
 class Config {
@@ -20,7 +21,7 @@ class Config {
       { key: 'User-Agent', value: 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0' },
       { key: 'Accept', value: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' },
       { key: 'Accept-Language', value: 'en-US,en;q=0.7,de-DE,de;q=0.3' },
-      { key: 'Accept-Encoding', value: 'gzip, deflate' },
+      { key: 'Accept-Encoding', value: 'identity;q=0.9, deflate;q=0.5, gzip;q=0.1, *;q=0' },
       { key: 'DNT', value: 1 },
       { key: 'Upgrade-Insecure-Requests', value: 1 },
       { key: 'Cache-Control', value: 'max-age=0' }
@@ -31,24 +32,41 @@ class Config {
 
     /** @type {{url: string, method?: string, protocolVersion?: string, headers?: {key: string, value: string | number | string[]}[], body?: path.ParsedPath | string, onHeaders?: function, onBody?: function, onError?: function, fetchBody?: boolean, connectionTimeoutMs?: number}[]} */
     this.tasks = [
-      { url: 'http://www.microsoft.com' }, // minimal required definition
+      // { url: 'http://www.microsoft.com' }, // minimal required definition
       {
-        url: 'http://compilenix.org',
+        url: 'https://compilenix.org',
         protocolVersion: '1.1',
-        // headers: [
-        //   // - these headers does overwrite default headers
-        //   // - if a header is 'null' or 'undefined' it will be removed
-        //   { key: 'User-Agent', value: 'NodeJS http-tester' }
-        // ],
+        headers: [
+          // - these headers does overwrite default headers
+          // - if a header is 'null' or 'undefined' it will be removed
+          { key: 'Upgrade-Insecure-Requests', value: null }
+        ],
         // body: path.parse('./LICENSE'),
         fetchBody: true,
-        onHeaders: (/** @type {http.IncomingMessage} */ res) => {
-          console.dir(res.rawHeaders)
+        /**
+         * @function
+         * @param {http.IncomingMessage} res
+         * @returns {boolean} true indicates sucess
+         */
+        onHeaders: res => {
+          console.dir(res.headers)
+          return true
         },
-        onBody: (/** @type {string} */ res) => {
+        /**
+         * @function
+         * @param {string} raw contains the parsed response body
+         * @param {CheerioStatic} $
+         * @see https://cheerio.js.org/
+         */
+        onBody: (res, $) => {
           console.dir(res)
+          console.dir($)
         },
-        onError: (/** @type {Error} */ error) => {
+        /**
+         * @function
+         * @param {Error} error
+         */
+        onError: error => {
           console.dir(error)
         }
       }
--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const https = require('https')
 const http2 = require('http2')
 const { URL } = require('url')
 const path = require('path')
+const zlib = require('zlib')
+const { Readable } = require('stream')
 
 const fs = require('fs-extra')
 const moment = require('moment')
 const Slack = require('slack-node')
 const punycode = require('./node_modules/punycode')
+const cheerio = require('cheerio')
+const StreamToString = require('stream-to-string')
 
-if (!fs.existsSync('./config.js')) {
-  fs.copySync('./config.example.js', './config.js')
-}
+// if (!fs.existsSync('./config.js')) {
+fs.copySync('./config.example.js', './config.js')
+// }
 
 let config = require('./config.js')
 let slack = new Slack()
@@ -91,6 +95,173 @@ function addMessage (message, method, uri, level = 'error') {
   })
 }
 
+/**
+ * @param {http.IncomingMessage} res
+ * @param {{url: string, method?: string, protocolVersion?: string, headers?: {key: string, value: string | number | string[]}[], body?: path.ParsedPath | string, onHeaders?: function, onBody?: function, onError?: function, fetchBody?: boolean, connectionTimeoutMs?: number}} task
+ * @param {function} resolve
+ * @param {function} reject
+ */
+function handleHttpClientResponse (res, task, resolve, reject) {
+  setupResolveResponseError(res, task, resolve)
+  console.log(`${task.url} -> ${res.statusCode}`)
+  if (invokeOnHeadersCallback(res, task) && res.statusCode >= 400) addMessage(`Status code does not indicate success: ${res.statusCode}`, task.method || config.defaultMethod, task.url)
+  setupResolveResponseBody(res, task, resolve)
+}
+
+/**
+ * @param {http.ClientRequest} req
+ * @param {{url: string, method?: string, protocolVersion?: string, headers?: {key: string, value: string | number | string[]}[], body?: path.ParsedPath | string, onHeaders?: function, onBody?: function, onError?: function, fetchBody?: boolean, connectionTimeoutMs?: number}} task
+ * @param {function} resolve
+ */
+function setupRequest (req, task, resolve) {
+  req.setNoDelay(true)
+  req.setSocketKeepAlive(false)
+  req.setTimeout(task.connectionTimeoutMs || config.defaultConnectionTimeoutMs || 2500)
+
+  if (config.defaultHeaders && config.defaultHeaders.length > 0) {
+    for (let index = 0; index < config.defaultHeaders.length; index++) {
+      const header = config.defaultHeaders[index]
+      if (header.value === null || header.value === undefined) {
+        req.removeHeader(header.key)
+      } else {
+        req.setHeader(header.key, header.value)
+      }
+    }
+  }
+
+  if (task.headers && task.headers.length > 0) {
+    for (let index = 0; index < task.headers.length; index++) {
+      const header = task.headers[index]
+      if (header.value === null || header.value === undefined) {
+        req.removeHeader(header.key)
+      } else {
+        req.setHeader(header.key, header.value)
+      }
+    }
+  }
+
+  setupRequestError(req, task, resolve)
+}
+
+/**
+ * @param {http.ClientRequest} req
+ * @param {{url: string, method?: string, protocolVersion?: string, headers?: {key: string, value: string | number | string[]}[], body?: path.ParsedPath | string, onHeaders?: function, onBody?: function, onError?: function, fetchBody?: boolean, connectionTimeoutMs?: number}} task
+ * @param {function} resolve
+ */
+function setupRequestError (req, task, resolve) {
+  req.on('error', error => {
+    if (task.onError) task.onError(error)
+    resolve()
+  })
+}
+
+/**
+ * @param {http.ClientRequest} req
+ * @param {{url: string, method?: string, protocolVersion?: string, headers?: {key: string, value: string | number | string[]}[], body?: path.ParsedPath | string, onHeaders?: function, onBody?: function, onError?: function, fetchBody?: boolean, connectionTimeoutMs?: number}} task
+ */
+function sendRequestBodySync (req, task) {
+  // @ts-ignore here is task.body.length always "falsy" or a string
+  if (task.body && task.body.length > 0) {
+    try {
+      req.write(task.body)
+    } catch (error) {
+      console.dir(error)
+      // TODO: handle?
+    }
+  } else {
+    req.end()
+  }
+}
+
+/**
+ * @param {http.IncomingMessage} res
+ * @param {{url: string, method?: string, protocolVersion?: string, headers?: {key: string, value: string | number | string[]}[], body?: path.ParsedPath | string, onHeaders?: function, onBody?: function, onError?: function, fetchBody?: boolean, connectionTimeoutMs?: number}} task
+ * @param {function} resolve
+ */
+function setupResolveResponseError (res, task, resolve) {
+  res.on('error', error => {
+    if (task.onError) {
+      task.onError(error)
+    } else {
+      console.dir(error)
+    }
+    resolve()
+  })
+}
+
+/**
+ * @param {http.IncomingMessage} res
+ * @param {{url: string, method?: string, protocolVersion?: string, headers?: {key: string, value: string | number | string[]}[], body?: path.ParsedPath | string, onHeaders?: function, onBody?: function, onError?: function, fetchBody?: boolean, connectionTimeoutMs?: number}} task
+ */
+function invokeOnHeadersCallback (res, task) {
+  if (task.onHeaders) {
+    task.onHeaders(res)
+  }
+}
+
+/**
+ * @param {http.IncomingMessage} res
+ * @param {{url: string, method?: string, protocolVersion?: string, headers?: {key: string, value: string | number | string[]}[], body?: path.ParsedPath | string, onHeaders?: function, onBody?: function, onError?: function, fetchBody?: boolean, connectionTimeoutMs?: number}} task
+ * @param {function} resolve
+ */
+function setupResolveResponseBody (res, task, resolve) {
+  if (!task.fetchBody) {
+    res.on('end', () => {
+      resolve()
+    })
+    res.emit('end')
+    return
+  }
+
+  if (res.headers['content-encoding']) {
+    switch (res.headers['content-encoding'].toLowerCase()) {
+      case 'deflate':
+      case 'gzip':
+        break
+      default:
+        res.setEncoding('utf8')
+        break
+    }
+  } else {
+    res.setEncoding('utf8')
+  }
+
+  let readable = new Readable()
+  readable._read = () => {}
+  res.on('data', chunk => {
+    readable.push(chunk)
+  })
+
+  res.on('end', async () => {
+    if (!task.onBody) {
+      resolve()
+      return
+    }
+
+    readable.push(null)
+    let rawBody = ''
+    if (res.headers['content-encoding']) {
+      switch (res.headers['content-encoding'].toLowerCase()) {
+        case 'deflate':
+          rawBody = await StreamToString(readable.pipe(zlib.createInflate()))
+          break
+        case 'gzip':
+          rawBody = await StreamToString(readable.pipe(zlib.createGunzip()))
+          break
+        default:
+          rawBody = await StreamToString(readable)
+          break
+      }
+    } else {
+      rawBody = await StreamToString(readable)
+    }
+
+    let $ = cheerio.load(rawBody)
+    task.onBody(rawBody, $)
+    resolve()
+  })
+}
+
 async function run () {
   for (let index = 0; index < config.tasks.length; index++) {
     const task = config.tasks[index]
@@ -111,91 +282,33 @@ async function run () {
       task.body = await fs.readFile(`${parsedPath.dir}${path.sep}${parsedPath.base}`, { encoding: 'utf8' })
     }
 
+    let clientRequest = {
+      timeout: task.connectionTimeoutMs || config.defaultConnectionTimeoutMs || 2500,
+      href: url.href,
+      method: method,
+      host: url.host,
+      hostname: url.hostname,
+      pathname: url.pathname,
+      search: url.search,
+      hash: url.hash
+    }
+
     await new Promise(async (resolve, reject) => {
       switch (task.protocolVersion || config.defaultProtocolVersion || '1.1') {
         case '1.0':
         case '1.1':
           if (isEncrypted) {
-            // TODO: handle
-            console.error(new Error('Not implemented'))
+            let req = https.request(clientRequest, res => {
+              handleHttpClientResponse(res, task, resolve, reject)
+            })
+            setupRequest(req, task, resolve)
+            sendRequestBodySync(req, task)
           } else {
-            let rawData = ''
-            let req = new http.ClientRequest({
-              timeout: task.connectionTimeoutMs || config.defaultConnectionTimeoutMs || 2500,
-              href: url.href,
-              method: method,
-              host: url.host,
-              hostname: url.hostname,
-              pathname: url.pathname,
-              search: url.search,
-              hash: url.hash
-            }, res => {
-              res.setEncoding('utf8')
-              res.on('error', error => {
-                if (task.onError) {
-                  task.onError(error)
-                } else {
-                  console.dir(error)
-                }
-                resolve()
-              })
-              if (task.onHeaders) {
-                task.onHeaders(res)
-              }
-
-              console.log(`${task.url} -> ${res.statusCode}`)
-
-              if (res.statusCode >= 400) addMessage(`Status code does not indicate success: ${res.statusCode}`, task.method || config.defaultMethod, task.url)
-
-              if (task.fetchBody) {
-                res.on('data', chunk => {
-                  rawData += chunk
-                })
-                res.on('end', () => {
-                  if (task.onBody) task.onBody(rawData)
-                  resolve()
-                })
-              } else {
-                res.on('end', () => {
-                  resolve()
-                })
-                res.emit('end')
-              }
+            let req = http.request(clientRequest, res => {
+              handleHttpClientResponse(res, task, resolve, reject)
             })
-
-            req.setNoDelay(true)
-            req.setSocketKeepAlive(false)
-            req.setTimeout(task.connectionTimeoutMs || config.defaultConnectionTimeoutMs || 2500)
-
-            if (config.defaultHeaders && config.defaultHeaders.length > 0) {
-              for (let index = 0; index < config.defaultHeaders.length; index++) {
-                const header = config.defaultHeaders[index]
-                req.setHeader(header.key, header.value)
-              }
-            }
-
-            if (task.headers && task.headers.length > 0) {
-              for (let index = 0; index < config.defaultHeaders.length; index++) {
-                const header = config.defaultHeaders[index]
-                req.setHeader(header.key, header.value)
-              }
-            }
-
-            req.on('error', error => {
-              if (task.onError) task.onError(error)
-              resolve()
-            })
-            // @ts-ignore here is task.body.length always "falsy" or a string
-            if (task.body && task.body.length > 0) {
-              try {
-                req.write(task.body)
-              } catch (error) {
-                console.dir(error)
-                // TODO: handle?
-              }
-            } else {
-              req.end()
-            }
+            setupRequest(req, task, resolve)
+            sendRequestBodySync(req, task)
           }
           break
         case '2.0':
