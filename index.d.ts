dfe8ccebb707820e8f41ffef4487a6476c841711
Compilenix <compilenix@gmail.com>
2018-06-11 03:03:36 +0200

    add type definition for a Task

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,16 @@
 /// <reference path="typings/index.d.ts" />
+
+interface Task {
+  url: string,
+  method?: string,
+  protocolVersion?: string,
+  headers?: { key: string, value: string | number | string[] }[],
+  body?: path.ParsedPath | string,
+  onHeaders?: (res: http.IncomingMessage) => string[] | void,
+  onBody?: (raw: string, $: CheerioStatic) => string[] | void,
+  onError?: (error: Error) => void,
+  fetchBody?: boolean,
+  headerPolicies?: Array<string | { [x: string]: boolean }>,
+  bodyPolicies?: Array<string | { [x: string]: boolean }>,
+  connectionTimeoutMs?: number
+}
